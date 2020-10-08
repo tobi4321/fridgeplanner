2d1eee7f7fe2f7a771e00f571863da2a14e240ed
tobi4321 <tobias.buehler2@outlook.de>
2020-10-08 10:17:04 +0200

    Added ASP.Net Core MVC Template

---
+++ FridgePlanner/FridgePlanner/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FridgePlanner.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
