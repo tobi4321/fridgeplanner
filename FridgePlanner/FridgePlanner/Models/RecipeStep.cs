08a42a5308682464b7c1de9e637d77cb2aedf075
tobi4321 <tobias.buehler2@outlook.de>
2020-10-09 16:44:30 +0200

    Changed Styling and added new Side Navbar
    Added StepNumber to RecipeStep

--- FridgePlanner/FridgePlanner/Models/RecipeStep.cs
+++ FridgePlanner/FridgePlanner/Models/RecipeStep.cs
@@ -8,6 +8,7 @@ namespace FridgePlanner.Models
     public class RecipeStep
     {
         public int RecipeStepId { get; set; }
+        public int StepNumber { get; set; }
         public string Name { get; set; }
         public string Text { get; set; }
     }
