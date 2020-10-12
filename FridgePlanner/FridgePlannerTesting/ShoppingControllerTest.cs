795618b63eac2e4122af08a3d6a6d781c986535d
tobi4321 <tobias.buehler2@outlook.de>
2020-10-12 14:59:19 +0200

    Added Unit Test for ShoppingController
    Changed Database for HomeControllerTest

--- FridgePlanner/FridgePlannerTesting/ShoppingControllerTest.cs
+++ FridgePlanner/FridgePlannerTesting/ShoppingControllerTest.cs
@@ -1,25 +1,26 @@
-using FridgePlanner.Controllers;
+﻿using FridgePlanner.Controllers;
 using FridgePlanner.Models;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using Xunit;
-using System.Linq;
-using Microsoft.AspNetCore.Mvc;
 using FridgePlanner.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
 
 namespace FridgePlannerTesting
 {
-    public class HomeControllerTest
+    public class ShoppingControllerTest
     {
-        private HomeController controller { get; }
+        private ShoppingController controller { get; }
         DataBaseContext context { get; }
-        FridgeItem item { get; }
-        Recipe recipe { get; }
+        ShoppingListItem item { get; }
 
-        public HomeControllerTest()
+        public ShoppingControllerTest()
         {
             // configure DB-Options ---> A Test db on your local DB will be used
-            var optionsBuilder = new DbContextOptionsBuilder<DataBaseContext>().UseSqlServer(@"Data Source=(LocalDb)\MSSQLLocalDB;Initial Catalog=FridgePlannerTesting1;  Integrated Security=True");
+            var optionsBuilder = new DbContextOptionsBuilder<DataBaseContext>().UseSqlServer(@"Data Source=(LocalDb)\MSSQLLocalDB;Initial Catalog=FridgePlannerTesting2;  Integrated Security=True");
 
             // create the context object to ensure the db operations go through
             context = new DataBaseContext(optionsBuilder.Options);
@@ -30,38 +31,23 @@ namespace FridgePlannerTesting
             context.Database.EnsureCreated();
 
             // create a FridgeItem for testing purposes
-            item = new FridgeItem()
+            item = new ShoppingListItem()
             {
                 Name = "Tomate",
                 Amount = 1,
-                Unit = "Kg",
-                ExpiryDate = new System.DateTime(2020, 10, 15)
-            };
-
-
-            // create a Recipe for testing purposes
-            recipe = new Recipe()
-            {
-                Name = "TestRezept",
-                Description = "Ein einfaches Test Rezept"
+                Unit = "Kg"
             };
 
             if (!context.FridgeItems.Any())
             {
-                context.FridgeItems.Add(item);
-            }
-
-            if (!context.Recipes.Any())
-            {
-                context.Recipes.Add(recipe);
+                context.ShoppingListItems.Add(item);
             }
 
             context.SaveChanges();
 
-            controller = new HomeController(context);
+            controller = new ShoppingController(context);
 
         }
-
         [Fact]
         public void Index_ReturnsAViewResult()
         {
@@ -75,7 +61,10 @@ namespace FridgePlannerTesting
             //var recipeId = context.Recipes.First().RecipeId;
             //context.Recipes.Where(r => r.RecipeId == recipeId).First().RecipeItems.Add(recipeItem);
             //context.SaveChanges();
+            string qrCodeText = "ShoppingListe \n";
+            foreach (ShoppingListItem item in context.ShoppingListItems) { qrCodeText += "- " + item.Name + "   " + item.Amount + item.Unit + " \n"; }
 
+            byte[] qrCode = QRGenerator.GenerateQR(qrCodeText);
 
             // Act
             var result = controller.Index();
@@ -85,13 +74,12 @@ namespace FridgePlannerTesting
             Assert.IsType<ViewResult>(result);
 
             var viewResult = result as ViewResult;
-            var model = viewResult.Model as IndexViewModel;
+            var model = viewResult.Model as ShoppingViewModel;
 
-            Assert.NotNull(model.FridgeItems);
-            //Assert.NotNull(model.Recipes);
-            Assert.Contains(item,model.FridgeItems); 
-            //Assert.Contains(recipe,model.Recipes);
-
+            Assert.NotNull(model.ShoppingItems);
+            Assert.NotNull(model.QrCodeData);
+            Assert.Contains(item, model.ShoppingItems);
+            Assert.Equal(qrCode,model.QrCodeData);
         }
     }
 }
